ad5c2dc937da149ea204cf22dd600f517010e701
iamayushdas <ayush24das@gmail.com>
2021-04-26 23:06:03 +0530

    login page done

--- spotify-clone-app/src/App.js
+++ spotify-clone-app/src/App.js
@@ -1,10 +1,34 @@
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Login from './Login';
+import { getTokenFromUrl } from './spotify';
 
 function App() {
+  const [token, setToken] = useState(null);
+  //  run code based on a given condition
+  useEffect(() => {
+    // code...
+    const hash = getTokenFromUrl();
+    window.location.hash= "";
+    const _token = hash.access_token;
+
+    if (_token) {
+      setToken(_token)
+    }
+
+    console.log('I HAVE A TOKEN >>> ', token);
+  }, []);
+  
   return (
     <div className="App">
-     <Login />
+      {
+        token ? (
+          <h1>I am logged in</h1>
+        ) : (
+          <Login />
+        )
+      }
+     
     </div>
   );
 }
