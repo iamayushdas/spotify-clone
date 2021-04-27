62968ecffa46d815c4adc8492671d8556cffa373
iamayushdas <ayush24das@gmail.com>
2021-04-27 14:41:30 +0530

    web api added

--- spotify-clone-app/src/App.js
+++ spotify-clone-app/src/App.js
@@ -2,6 +2,9 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 import Login from './Login';
 import { getTokenFromUrl } from './spotify';
+import SpotifyWebApi from "spotify-web-api-js"
+
+const spotify = new SpotifyWebApi();
 
 function App() {
   const [token, setToken] = useState(null);
@@ -13,12 +16,18 @@ function App() {
     const _token = hash.access_token;
 
     if (_token) {
-      setToken(_token)
+      setToken(_token);
+
+      spotify.setAccessToken(_token);
+
+      spotify.getMe().then(user => {
+        console.log('USER', user);
+      })
     }
 
     console.log('I HAVE A TOKEN >>> ', token);
   }, []);
-  
+
   return (
     <div className="App">
       {
@@ -28,7 +37,7 @@ function App() {
           <Login />
         )
       }
-     
+
     </div>
   );
 }
